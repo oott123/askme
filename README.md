30ed4138987731f4462af7549f51a822e2746a0a
oott123 <git@public.oott123.com>
2020-03-06 15:21:44 +0800

    chore: add readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# AskMe!
+
+<center>一个简洁的自建提问箱系统。</center>
+
+## 特色
+
+* 简洁轻量、依赖少
+* 开源、无追踪，不记录提交者身份
+* 使用 PHP+SQLite 数据库，部署方便
+
+## 预览图
+
+| ![](./promotion/index.jpg) | ![](./promotion/submit.jpg) | ![](./promotion/single.jpg) | ![](./promotion/answerme.jpg) | 
+|---|---|---|---|
+| 首页 | 问题提交 | 查看历史 | 回答 |
+
+## 安装
+
+### 下载
+
+直接下载源码或 Release 源码包即可。
+
+### 安装
+
+复制 `config.example.php` 文件为 `config.php`，并修改其中选项。其中 `site_secret` 可以使用任意文本随意生成，建议在 16 个字符以上。
+
+## 授权协议
+
+[AGPLv3](./LICENSE)
--- config.example.php
+++ config.example.php
@@ -4,17 +4,17 @@
   ini_set('display_errors', 'no');
 
   class AskMeConfig {
-    public static $site_title = '我的提问箱';
-    public static $site_advertise = '访问 example.com 即可向我匿名提问！';
-    public static $site_avatar = 'https://http.cat/404';
+    public static $site_title = '我的提问箱'; // 提问箱标题
+    public static $site_advertise = '访问 example.com 即可向我匿名提问！'; // 生成图片上显示的宣传文字
+    public static $site_avatar = 'https://avatars3.githubusercontent.com/in/15368?s=64&v=4';  // 站点头像
 
-    public static $submit_success = '你的问题我已经收到了，回复之后我会在微博或者推特上发布！<br>你也可以常来看看我的提问箱，看看有没有被回复~';
+    public static $submit_success = '你的问题我已经收到了，回复之后我会在微博或者推特上发布！<br>你也可以常来看看我的提问箱，看看有没有被回复~';  // 提交成功后显示的文字，支持 HTML
 
-    public static $recent_answers = 6;
-    public static $list_answers = 10;
+    public static $recent_answers = 6;  // 最近回答的列表显示的数量
+    public static $list_answers = 10; // 列表显示所有问题时一页的数量
 
-    public static $database = 'sqlite:../private/data.sqlite3';
+    public static $database = 'sqlite:../private/data.sqlite3'; // sqlite 路径，不要放到站点目录下！
 
-    public static $admin_password = '';
-    public static $site_secret = '';
+    public static $admin_password = ''; // 管理员密码
+    public static $site_secret = '';  // 站点 secret，随便生成，越长越好
   }
