fffd496d6663a1951ebc8496a7f89d2fff4faa30
oott123 <git@public.oott123.com>
2020-03-06 13:44:30 +0800

    deleted single view

--- view.php
+++ view.php
@@ -6,5 +6,15 @@
   $view_layout = 'single.php';
   $view_all = false;
   $question = AskMeDB::$instance->get($id);
-  $question_list = AskMeDB::$instance->recent(AskMeConfig::$recent_answers, $id);
+
+  if (empty($question) || $question['deleted_at'] > 0) {
+    $question = null;
+    $view_layout = 'message.php';
+    $message_title = '找不到回答';
+    $message_body = '回答不存在，或者问题已经被删除了 :(';
+    header('HTTP/1.1 404 Not Found');
+  } else {
+    $question_list = AskMeDB::$instance->recent(AskMeConfig::$recent_answers, $id);
+  }
+
   include 'views/layout.php';
