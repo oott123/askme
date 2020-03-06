a0fa3cc56ea2bf527bc1169a95c24ae4bae5bd81
oott123 <git@public.oott123.com>
2020-03-06 19:14:59 +0800

    fix: more clear screenshots

--- scripts/admin.js
+++ scripts/admin.js
@@ -6,17 +6,34 @@
     $btn.addClass('loading');
     $('.img-view').html('');
 
-    domtoimage.toJpeg($dom[0], { quality: 0.95 })
-    .then(function (dataUrl) {
-      var link = document.createElement('a');
-      link.download = Date.now() + '.jpg';
-      link.href = dataUrl;
-      link.click();
-      $('.img-view').append($('<img>').attr('src', dataUrl));
-    })
-    .finally(function () {
-      $btn.removeClass('loading');
-      $dom.removeClass('screenshot');
+
+    requestAnimationFrame(function () {
+      var scale = 3;
+      var height = $dom[0].offsetHeight;
+      var width = $dom[0].offsetWidth;
+
+      domtoimage.toJpeg($dom[0], {
+        quality: 0.95,
+        height: height * scale,
+        width: width * scale,
+        style: {
+          transform: 'scale(' + scale + ')',
+          transformOrigin: 'top left',
+          width: width + 'px',
+          height: height + 'px'
+        }
+      })
+      .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = Date.now() + '.jpg';
+        link.href = dataUrl;
+        link.click();
+        $('.img-view').append($('<img style="max-width: 100%; border: 1px solid #ccc;">').attr('src', dataUrl));
+      })
+      .finally(function () {
+        $btn.removeClass('loading');
+        $dom.removeClass('screenshot');
+      });
     });
   });
 })();
